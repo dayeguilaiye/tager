a917c6650edeb4544ab977ca3c3c9dac358263a9
dayeguilaiye <979014041@qq.com>
2017-10-10 18:23:09 +0800

    初步建立服务端框架，剧本语言起草

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './drama/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+'.txt')
+  }
+})
+var upload = multer({storage: storage})
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log("aaa");
+  res.render('upload', { title: 'Express' });
 });
 
+router.post('/uploadFile',upload.single('drama'), function(req, res, next) {
+	console.log(req.file.path);
+  res.send('success');
+});
 
 module.exports = router;
